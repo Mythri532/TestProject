f10e0522d12ad01f99db5b95f6ed831d5ce8fed0
mythri_g <mythri_g@THBSSTLPT-0321.thbs.india.com>
2025-05-02 17:08:04 +0530

    push

---
+++ test1/src/test1/Sample.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+	int num=12;
+	
+	if (num%2==0) {
+		System.out.println("num is odd");
+	}
+		else 
+		{
+			System.out.println("num is even");	
+		}
+	}
+	}
+
+
