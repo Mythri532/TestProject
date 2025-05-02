57f1ae5aee0aa639b7c210b488b7db5acdbdb284
mythri_g <mythri_g@THBSSTLPT-0321.thbs.india.com>
2025-05-02 17:11:25 +0530

    hi

---
+++ test1/src/test1/Sample3.java
@@ -0,0 +1,27 @@
+package test1;
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=0;
+		 for(int i=1;i<=100;i++)
+	      {
+	    	 if (i%2==1) {
+	    		 
+	    		 count=count+1;
+	    	
+	    		 System.out.println("numbers are: " + i);
+
+				
+	    	 }
+	 
+	    		 
+	    		 
+	    	 }
+		 
+		 System.out.println("count of odd numbers " +count);
+		 
+	}
+
+}
