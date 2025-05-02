7221f53b3294535181c0032be8de703e256e8a78
mythri_g <mythri_g@THBSSTLPT-0321.thbs.india.com>
2025-05-02 17:09:19 +0530

    hi

---
+++ test1/src/test1/Sample1.java
@@ -0,0 +1,23 @@
+package test1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      for(int i=1;i<=100;i++)
+      {
+    	 if (i%2==1) {
+    	
+    		 System.out.println("numbers are: " + i);
+
+			
+    	 }
+ 
+    		 
+    		 
+    	 }
+      }
+		
+	}
+
+
